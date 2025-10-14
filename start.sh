bc1630c8dd555d5e8dd52cf67cd9cf42c9caa430
Sandra_G <sandra.gedig@iu-study.org>
2025-10-14 09:51:24 +0200

    Update start.sh to run MLflow UI

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# Start Flask App
+
+# Start Flask App auf 8000
 echo "Starting Flask API..."
 python app.py &
+
+# Start MLflow UI auf Port 5001
+echo "Starting MLflow UI..."
+mlflow ui --host 0.0.0.0 --port 5001 &
 
 # Start Drift Watchdog
 echo "Starting Drift Watchdog..."
 python drift_watchdog.py &
+
 wait
